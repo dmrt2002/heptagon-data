470d7f34383f68ef657a22c273d4e56d2958008c
R Tushar <dmrtushar@gmail.com>
2022-08-04 21:26:36 +0530

    eff

--- frontend/src/views/Results.vue
+++ frontend/src/views/Results.vue
@@ -9,19 +9,94 @@
       class="align-items-center flex justify-content-center fadein animation-duration-1500 align-center"
     >
       <div>
-        <div class="align-items-center flex justify-content-center">
-          <img class="img-fluid ml-2" :src="require(`../assets/${url}`)" alt="" />
+        <h1 class="text-center main-head">Persona Blend</h1>
+        <div class="grid">
+          <div class="col-3">
+            <img
+              :src="require('../assets/explorer.png')"
+              alt="Image"
+              class="image"
+            />
+            <div class="text-center persona">Data explorer</div>
+            <div class="text-center perc">{{personas.one}}%</div>
+          </div>
+          <div class="col-3">
+            <img
+              :src="require('../assets/operator.png')"
+              alt="Image"
+              class="image"
+            />
+            <div class="text-center persona">Data operator</div>
+            <div class="text-center perc">{{personas.two}}%</div>
+          </div>
+          <div class="col-3">
+            <img
+              :src="require('../assets/interpreter.png')"
+              alt="Image"
+              class="image"
+            />
+            <div class="text-center persona">Data interpreter</div>
+            <div class="text-center perc">{{personas.three}}%</div>
+          </div>
+          <div class="col-3">
+            <img
+              :src="require('../assets/influencer.png')"
+              alt="Image"
+              class="image"
+            />
+            <div class="text-center persona">Data influencer</div>
+            <div class="text-center perc">{{personas.four}}%</div>
+          </div>
         </div>
-        <h1 class="head">{{ title }}</h1>
-        <p class="text-align font-bold">{{ text }}</p>
+        <div class="heading">Data Explorer</div>
+        <p class="information">
+          Your daily job involves working with some numbers. You occasionally
+          use tool like excel sheet to create surveys/lists/reports etc. You
+          share data inputs to others and the quality of that data is important
+          for the department You often hear terms like “Big Data” “Analytics”
+          “Insights” etc and curious to update yourself. You want to know how
+          you can become more efficient and avoid reworks in tasks involving
+          data
+        </p>
+        <div class="heading">Data Operator</div>
+        <p class="information">
+          A significant part of your work involves collecting/collating data
+          from different sources. You may work with sources like CRM / HRM /
+          Supply ERP / Surveys etc. You make reports which is then used by you
+          or mostly by someone else for further analysis. You make these data
+          sheets in excel and often have a set calendar to share with others
+        </p>
+        <div class="heading">Data Interpreter</div>
+        <p class="information">
+          Your work involves reading various data tables / reports / graphs /
+          dashboards. You often are looking at solving tactical problems in
+          business. You generate reports describing What has happened in your
+          line of work From a given data set you often try to identify what is
+          causing a trend and come up with call to action / suggestions /
+          recommendations based on your analysis
+        </p>
+        <div class="heading">Data Influencer</div>
+        <p class="information" id="lastp">
+          Your daily job involves working with some numbers. You occasionally
+          use tool like excel sheet to create surveys/lists/reports etc. You
+          share data inputs to others and the quality of that data is important
+          for the department You often hear terms like “Big Data” “Analytics”
+          “Insights” etc and curious to update yourself. You want to know how
+          you can become more efficient and avoid reworks in tasks involving
+          data
+        </p>
         <div class="align-items-center flex justify-content-center">
-            <a :href="`https://hept-data.herokuapp.com/${pdfUrl}`" download="pathway.pdf" class="cta cursor-pointer">
-              <span>Dowload your learning pathway</span>
-              <svg width="13px" height="10px" viewBox="0 0 13 10">
-                <path d="M1,5 L11,5"></path>
-                <polyline points="8 1 12 5 8 9"></polyline>
-              </svg>
-            </a>
+          <a
+            :href="`https://hept-data.herokuapp.com/${pdfUrl}`"
+            download="pathway.pdf"
+            class="cta cursor-pointer"
+          >
+            <span>Dowload your learning pathway</span>
+            <svg width="13px" height="10px" viewBox="0 0 13 10">
+              <path d="M1,5 L11,5"></path>
+              <polyline points="8 1 12 5 8 9"></polyline>
+            </svg>
+          </a>
         </div>
       </div>
     </div>
@@ -31,21 +106,37 @@
 <script>
 import { useStore } from "vuex";
 import images from "../images";
-import axios from 'axios'
-import { onMounted } from '@vue/runtime-core';
+import axios from "axios";
+import { onMounted, reactive } from "@vue/runtime-core";
 export default {
   setup() {
     let store = useStore();
     let options = store.getters.getOptions;
     let length = options.length;
+    const personas = reactive({
+      one: null,
+      two: null,
+      three: null,
+      four: null
+    })
+    const count = {};
     let link = [
       "https://raw.githubusercontent.com/dmrt2002/images/696ad09814061b0a6d1ff1653ac680969870b760/heptagon.svg",
     ];
+    for (const element of options) {
+      if (count[element]) {
+        count[element] += 1;
+      } else {
+        count[element] = 1;
+      }
+    }
+    personas.one = count[1]/10 * 100
+    personas.two = count[2]/10 * 100
+    personas.three = count[3]/10 * 100
+    personas.four = count[4]/10 * 100
+    console.log(personas.one)
     const mostFrequent = (arr, n) => {
       arr.sort();
-
-      // find the max frequency using linear
-      // traversal
       let max_count = 1,
         res = arr[0];
       let curr_count = 1;
@@ -63,19 +154,16 @@ export default {
       return res;
     };
     let imageId = mostFrequent(options, length);
-    let url = images[0].options[imageId - 1].imageUrl;
-    let text = images[0].options[imageId - 1].text;
-    let title = images[0].options[imageId - 1].title
-    let pdfUrl = images[0].options[imageId - 1].pdfUrl
-    let  publicPath = process.env.BASE_URL
+    const title = images[0].options[imageId - 1].title
+    let pdfUrl = images[0].options[imageId - 1].pdfUrl;
     onMounted(async () => {
       let param = {
         userId: store.getters.getId,
-        role: title
-      }
-       await axios.post('/user/role', param)
-    })
-    return { url, text, link, title, pdfUrl, publicPath};
+        role: title,
+      };
+      await axios.post("/user/role", param);
+    });
+    return {  link, pdfUrl , personas};
   },
 };
 </script>
@@ -85,6 +173,35 @@ export default {
 * {
   font-family: "Raleway", sans-serif;
 }
+#lastp {
+  margin-bottom: 10px;
+}
+.main-head {
+  font-size: 40px;
+}
+.persona {
+  font-size: 25px;
+  font-weight: 800;
+  padding-top: 10px;
+}
+.perc {
+  font-size: 25px;
+  padding-top: 10px;
+}
+.heading {
+  padding-top: 5vh;
+  text-align: center;
+  font-size: 35px;
+  padding-bottom: 15px;
+}
+
+.information {
+  margin-left: 5vw;
+  margin-right: 5vw;
+  font-size: 20px;
+  padding-bottom: 15px;
+}
+
 .background {
   background-color: rgba(241, 237, 246, 0.818);
   min-height: 100vh;
@@ -95,12 +212,20 @@ export default {
   height: 200px;
   margin-bottom: 4em;
 }
-
+.image {
+  width: 20vw;
+  height: 250px;
+}
+.text-center {
+  text-align: left;
+}
+.p-tag {
+  font-size: 25px;
+}
 .text-align {
   margin-left: 20vw;
   margin-right: 20vw;
   margin-bottom: 4em;
-  font-size: 16px;
 }
 .logo {
   height: 60px !important;
@@ -110,23 +235,25 @@ export default {
   align-content: center;
   justify-content: center;
   align-items: center;
-  min-height: calc(100vh - 100px);
 }
 .center-button {
-    font-size: 1.5rem !important;
-    margin-bottom: 10px;
+  font-size: 1.5rem !important;
+  margin-bottom: 10px;
 }
 .head {
   text-align: center;
   color: rgb(26, 26, 138);
   font-weight: 900;
+  font-size: 35px;
+  margin-bottom: 20px;
 }
 a {
   text-decoration: none;
   color: inherit;
 }
 
-.cta  , .cta-back {
+.cta,
+.cta-back {
   position: relative;
   margin: auto;
   padding: 19px 22px;
@@ -156,7 +283,8 @@ a {
   height: 56px;
   transition: all 0.3s ease;
 }
-.cta span, .cta-back span {
+.cta span,
+.cta-back span {
   position: relative;
   font-size: 14px;
   line-height: 18px;
@@ -165,7 +293,8 @@ a {
   text-transform: uppercase;
   vertical-align: middle;
 }
-.cta svg, .cta i {
+.cta svg,
+.cta i {
   position: relative;
   top: 0;
   margin-left: 10px;
@@ -177,14 +306,17 @@ a {
   transform: translateX(-5px);
   transition: all 0.3s ease;
 }
-.cta:hover:before, .cta-back:hover:before {
+.cta:hover:before,
+.cta-back:hover:before {
   width: 100%;
   background: lightblue;
 }
-.cta:hover svg, .cta-back i {
+.cta:hover svg,
+.cta-back i {
   transform: translateX(0);
 }
-.cta:active , .cta-back:active {
+.cta:active,
+.cta-back:active {
   transform: scale(0.96);
 }
 </style>
